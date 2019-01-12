6278228ca523f6cb6b0569eb733249c0898637a8
wuikhan <wuikhan@gmail.com>
2019-01-12 11:51:14 -0500

    tags

--- features/Test.feature
+++ features/Test.feature
@@ -1,11 +1,15 @@
 Feature: Test Facebook smoke scenario
 
-  Scenario Outline: Test login with valid credentials
+  @smoke
+  Scenario: Test login with valid credentials
     Given Open Chrome and start application
-    When I enter valid "<username>" and valid "password"
+    When I enter valid username and password
     Then user should be able to login successfully
     Then application should be closed
 
-    Examples: 
-      | username       | password  |
-      | abc@gmail.com  | password1 |
+  @smoke
+  Scenario: Test login with valid credentials
+    Given Open Chrome and start application
+    When I enter valid username and password
+    Then user should be able to login successfully
+    Then application should be closed
--- src/test/java/stepDefinition/smokeTest.java
+++ src/test/java/stepDefinition/smokeTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,26 +13,28 @@ public class smokeTest {
 
 	@Given("^Open Chrome and start application$")
 	public void open_Chrome_and_start_application()  {
-		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver2");
-		 driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+//		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver2");
+//		 driver = new ChromeDriver();
+//		driver.get("");
+		System.out.println("Chrome browser is open");
 	
 	}
 
-	@When("^I enter valid \"(.*?)\" and valid \"(.*?)\"$")
-	public void i_enter_valid_and_valid(String arg1, String arg2)  {
-		driver.findElement(By.id("username")).sendKeys(arg1);
-		driver.findElement(By.id("passsword")).sendKeys(arg2);
+	@When("^I enter valid username and password$")
+	public void i_enter_valid_username_and_password()  {
+		System.out.println("Chrome browser is open");
 	}
 
 	@Then("^user should be able to login successfully$")
-	public void user_should_be_able_to_login_successfully()  {
+	public void user_should_be_able_to_login_successfully() throws InterruptedException  {
+
 		System.out.println("I am logged in");
+		
 	}
 
 	@Then("^application should be closed$")
 	public void application_should_be_closed() {
-	driver.quit();
-	}
+		System.out.println("I am logged in");
+		}
 
 }
