2f5351cdc4d68328c2122d4384c022ce510d82de
wuikhan <wuikhan@gmail.com>
2019-01-12 12:00:05 -0500

    test

--- src/test/java/stepDefinition/smokeTest.java
+++ src/test/java/stepDefinition/smokeTest.java
@@ -23,6 +23,7 @@ public class smokeTest {
 	@When("^I enter valid username and password$")
 	public void i_enter_valid_username_and_password()  {
 		System.out.println("Chrome browser is open");
+		System.out.println("Chrome browser is open");
 	}
 
 	@Then("^user should be able to login successfully$")
